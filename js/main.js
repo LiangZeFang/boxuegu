785a02fdc987ee300638ebf826d307e294594c74
xiaowei <@xiaowei123.com>
2017-02-25 21:31:44 +0800

    脚本

---
+++ js/main.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Administrator on 2017/2/25.
+ */
+requirejs.config({
+    baseUrl:'/',
+    paths:{
+        //第三方库加载路径
+        jquery:'lib/jquery/jquery.min',
+        bootstrap:'lib/bootstrap/js/bootstrap.min',
+        //自己的加载路径
+        userList:'js/user/list',
+        userProfile:'js/user/profile'
+    },
+    shim:{
+        bootstrap:{
+            deps:['jquery']
+        }
+    }
+});
+require(['jquery','bootstrap']);
+//这里获取页面的pathname，然后对应的加载js
+(function(window){
+    var pathname = window.location.pathname;
+    switch (pathname){
+        case '/html/user/list.html':
+            require(['userList']);
+            break;
+        case '/html/user/profile.html':
+            require(['userProfile']);
+            break;
+    }
+})(window);
